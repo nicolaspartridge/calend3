ef783adb2ac039aa223bc9297b4c2faadfc34e02
Nicolas Partridge <nicolas@nicolass-MacBook-Air-3.local>
2022-06-03 11:30:36 -0700

    ADD: Test for adding appointments

--- contracts/Calend3.sol
+++ contracts/Calend3.sol
@@ -5,10 +5,6 @@ contract Calend3 {
     uint rate;
     address public owner;
     Appointment[] appointments;
-    
-    constructor() {
-        owner = msg.sender;
-    }
 
     struct Appointment {
       string title;
@@ -16,6 +12,10 @@ contract Calend3 {
       uint startTime;
       uint endTime;
       uint amontPaid;
+    }
+    
+    constructor() {
+        owner = msg.sender;
     }
 
     function getRate() public view returns (uint) {
@@ -25,5 +25,20 @@ contract Calend3 {
     function setRate(uint _rate) public {
         require(msg.sender == owner, "Only the owner can set the rate");
         rate = _rate;
+    }
+
+    function getAppointments() public view returns (Appointment[] memory) {
+      return appointments;
+    }
+
+    function createAppointment(string memory _title, uint _startTime, uint _endTime) public {
+      Appointment memory appointment;
+      appointment.title = _title;
+      appointment.attendee = msg.sender;
+      appointment.startTime = _startTime;
+      appointment.endTime = _endTime;
+      appointment.amontPaid = ((_endTime - _startTime) / 60 * rate);
+
+      appointments.push(appointment);
     }
 }
--- test/test.js
+++ test/test.js
@@ -1,27 +1,40 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Set rate", function () {
-  it("Should set the minutely rate", async function () {
-    const Contract = await ethers.getContractFactory("Calend3");
-    const contract = await Contract.deploy();
+describe("Calend3", function () {
+  let Contract, contract;
+  let owner, addr1, addr2;
+
+  beforeEach(async () => {
+    [owner, addr1, addr2] = await ethers.getSigners();
+    
+    Contract = await ethers.getContractFactory("Calend3");
+    contract = await Contract.deploy();
     await contract.deployed();
+  });
 
+  it("Should set the minutely rate", async function () {
     const tx = await contract.setRate(1000);
-
-    // wait until the transaction is mined
     await tx.wait();
 
-    // verify rate is set correctly
     expect(await contract.getRate()).to.equal(1000);
+  });
 
-    // get addresses
-    const [owner, addr1, addr2] = await ethers.getSigners();
-
-    // call setRate using a different account address
-    // this should fail since this address is not the owner
+  it("Should fail is non-owner sets rate", async function () {
     await expect(
       contract.connect(addr1).setRate(500)
     ).to.be.revertedWith('Only the owner can set the rate');
+  });
+
+  it("Should add two appointments", async function () {
+    const tx = await contract.createAppointment("Meeting with Nicolas", 1644143400, 1644150600);
+    tx.wait();
+
+    const tx2 = await contract.createAppointment("Second meeting with Nicolas", 1644160000, 1644170000);
+    tx2.wait();
+
+    const appointments = await contract.getAppointments();
+
+    expect(appointments.length).to.equal(2);
   });
 });
