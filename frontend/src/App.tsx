07638c04f51ffa9d14a7e65962baa4f5c604f816
Nicolas Partridge <nicolas@nicolass-MacBook-Air-3.local>
2022-06-03 10:39:20 -0700

    UPDATE: Front end logic to connect wallet

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+import detectEthereumProvider from "@metamask/detect-provider";
+
+import Calendar from "./components/Calendar";
 
 function App() {
+  const [account, setAccount] = useState<String>("");
+
+  useEffect(() => {
+    isConnected();
+  }, []);
+
+  const isConnected = async () => {
+    const provider: any = await detectEthereumProvider();
+    const accounts = await provider.request({
+      method: "eth_accounts",
+    });
+
+    if (accounts.length > 0) {
+      setAccount(accounts[0]);
+    } else {
+      console.log("No authorized accounts");
+    }
+  };
+
+  const connect = async () => {
+    try {
+      const provider: any = await detectEthereumProvider();
+      const accounts = await provider.request({
+        method: "eth_requestAccounts",
+      });
+
+      if (accounts.length > 0) {
+        setAccount(accounts[0]);
+      } else {
+        console.log("No accounts found");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>calend3</h1>
+      <div>web3 appointment scheduling</div>
+      {account.length ? (
+        <Calendar />
+      ) : (
+        <button onClick={connect}>Connect wallet</button>
+      )}
     </div>
   );
 }
